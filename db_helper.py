07f4709f3e33f4ae1bc81cd20a8d881affe07f21
erickolbusz <erickolbusz@gmail.com>
2015-01-11 17:04:38 -0500

    Started working on orders

--- db_helper.py
+++ db_helper.py
@@ -4,7 +4,9 @@ client = MongoClient()
 db = client.account_manager
 
 users = db.users
+orders = db.orders
 
+#-----USERNAMES-----
 def user_auth(username, password):
     return users.find({'username':username, 'password':password}).count() == 1
 
@@ -28,6 +30,29 @@ def get_data(username,data):
         if data in user:
             return user[data]
         return "No %s data for user %s."%(data,username)
+
+#-----ORDERS-----
+def order_creat(orderid,username, store, food, cost,
+                offer, preferredperiod,
+                otherperiods, instructions):
+    new = { 'orderid' : orderid,
+            'username' : username,
+            'store' : store,
+            'food' : food, #predefined list?
+            'cost' : cost, #calculated?
+            'offer' : offer,
+            'preferredperiod' : preferredperiod,
+            'otherperiods' : otherperiods,
+            'instructions' : instructions
+            }
+    orders.insert(new)
+    return "Order #%s created"%(orderid)
+
+def get_orders_store(store):
+    orders_store = orders.find({'store':store})
+    return [orders_store.count(), orders_store]
+
+
 
 
 
