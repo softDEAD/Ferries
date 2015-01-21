43c576e1cd098d0d49e8a3bb819631adb2d2c6ca
erickolbusz <erickolbusz@gmail.com>
2015-01-21 11:49:27 -0500

    Keep remembering to add things that I forget to add what does that even mean

--- db_helper.py
+++ db_helper.py
@@ -13,7 +13,7 @@ def user_auth(username, password): #string, string
 def user_exists(username): #string
     return users.find({'username':username}).count() > 0
 
-def user_creat(username, password, frees, lunch, rep, ordersplaced, ordersfulfilled, profilecomments): #string, string, list, int, int, int, int, list
+def user_creat(username, password): #string, string
     if (not user_exists(username)):
         new = { 'username' : username,
                 'password' : password,
@@ -23,11 +23,26 @@ def user_creat(username, password, frees, lunch, rep, ordersplaced, ordersfulfil
                 'ordersplaced' : 0,
 		'ordersfulfilled' : 0,
                 'profilecomments' : []
+                'pendingorders' : []
                 }
         users.insert(new)
         return "Registration successful"
     return "Registration failed; Username taken"
 
+def change_frees(username, frees): #string, list
+    user = users.find_one({'username':username})
+    users.update(
+        {'username' : username},
+        {"$set": {'frees': frees}},
+        upsert = True)
+
+def change_lunch(username, lunch): #string, int
+    user = users.find_one({'username':username})
+    users.update(
+        {'username' : username},
+        {"$set": {'lunch': lunch}},
+        upsert = True)
+    
 def get_data(username, data): #string, string
     user = users.find_one({'username':username})
     if (user != None):
@@ -56,29 +71,66 @@ def order_creat(orderid, username, store, food, cost,
             'takenby' : None #when taken by someone (if not None) it displays as 'Taken by %s'
             }
     orders.insert(new)
+
+    #adding 1 to orders created
+    ################################spammable??
+    user = users.find_one({'username':username})
+    users.update(
+        {'username' : username},
+        {"$set" : {'ordersplaced' : user['ordersplaced']+1}},
+        upsert = True)#just in case
     return "Order #%s created"%(orderid)
 
 def get_orders(stores, periods): #list, list
-    ret = []
+    ret = [] #returns list of orderids
     if len(stores) == 0:
         for period in periods:
-            ret.append(orders.find({'period':period}))
+            orders = orders.find({'period':period})
+        for order in orders:
+            ret.append(order['orderid'])
     elif len(periods) == 0:
         for store in stores:
-            ret.append(orders.find({'store':stores}))
+            orders = orders.find({'store':stores})
+            for order in orders:
+                ret.append(order['orderid'])
     else:
         for store in stores:
             for period in periods:
-                ret.append(orders.find({'store':store,
-                                        'period':period}))
+                orders = orders.find({'store':store,
+                                      'period':period})
+                for order in orders:
+                    ret.append(order['orderid'])
     return ret
 
 def order_fulfill(orderid): #int
     #username is the person who fulfilled order
     order = orders.findone({'orderid':orderid})
-    db.orders.remove(order)
-    
+    user = order['takenby']
+    if not user_exists(user):
+        return "Error, user not found"
+    users.update(
+        {'username' : username},
+        {"$set" : {'ordersfulfilled' : user['ordersfulfilled']+1}},
+        upsert = True)
+    orders.remove(order)
 
+def take_order(username, orderid): #string, string
+    user = users.find_one({'username':username})
+    order = orders.find_one({'orderid':orderid})
+    if order['takenby'] != None:
+        return "Order already taken by %s"%(order['takenby'])
+    pendingorders = user['pendingorders']
+    pendingorders.append(orderid)
+    users.update(
+        {'username' : username},
+        {"$set" : {'pendingorders' : pendingorders}},
+        upsert = True)
+    orders.update(
+        {'orderid' : orderid},
+        {"$set" : {'takenby' : username}},
+        upsert = True)
+    return "Order taken"
+    
 
 
 
