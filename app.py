9ec0df650e37aab572ee92a093b591840fa93f24
David Bang <davidbang2012@gmail.com>
2015-01-26 11:55:14 -0500

    stuff

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask,request,url_for,redirect,render_template, flash, session
-import json
+import json, urllib2
 from functools import wraps
 import db_helper as db
 import yelp
-try:
-    import urllib.request as urllib2
-    import urllib.parse as urllib
-except ImportError:
-    import urllib2
-    import urllib
 
 app=Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-global orderid
+orderid = 0
+id = 0
 
 @app.route("/", methods = ["POST", "GET"])
 def index():
@@ -33,7 +28,7 @@ def login():
         password = request.args.get("password")
         does_account_exist = db.user_auth(username, password);
         if (does_account_exist == True):
-            user = db.get_data (username);
+            user = db.get_data (username)
             return redirect("/profile")
         flash ("Invalid Username or Password")
         return redirect ("/")
@@ -48,7 +43,7 @@ def register():
     if (register == "Register"):
         username = request.args.get("username")
         password = request.args.get("password")
-        does_account_exist = db.user_auth(username, password);
+        does_account_exist = db.user_auth(username, password)
         if (does_account_exist == True):
             flash("Account already exists") #tried registering with taken username (None, None) is not a valid user/pass combo
             return redirect("/register")
@@ -56,7 +51,7 @@ def register():
             flash("Username too short, must be at least 6 characters") #username too short, None falls under here too
             return redirect("/register")
         else:
-            db.user_creat (username, password);
+            db.user_creat (username, password)
             flash("Successfully registered")
             return redirect ("/")
     return render_template ("register.html") #have a button that redirects to /
@@ -65,12 +60,13 @@ def register():
 def profile():
     if ('username' in session):
         username = session ['username']
-        return render_template ("profile.html", username = username, frees = db.get_data (username, frees), lunch = db.get_data (username, lunch), rep = db.get_data (username, rep), ordersp = db.get_data (username, ordersplaced), ordersf = db.get_data (username, ordersfulfilled), comments = db.get_data (username, comments));
+        return render_template ("profile.html", username = username, frees = db.get_data (username, frees), lunch = db.get_data (username, lunch), rep = db.get_data (username, rep), ordersp = db.get_data (username, ordersplaced), ordersf = db.get_data (username, ordersfulfilled), comments = db.get_data (username, comments))
     else:
         return redirect ("/login")
 
-@app.route("/oprofile", methods = ["POST", "GET"])
+@app.route("/oprofile/<username>", methods = ["POST", "GET"])
 def otherprofile(username): ##links from other user
+    
     return render_template ("profile.html", username = username, frees = db.get_data (username, frees), lunch = db.get_data (username, lunch), rep = db.get_data (username, rep), ordersp = db.get_data (username, ordersplaced), ordersf = db.get_data (username, ordersfulfilled), comments = db.get_data (username, comments));
 
 @app.route("/placeorder", methods = ["POST", "GET"])
@@ -93,22 +89,43 @@ def placeorder():
     else:
         return render_template ("/login")
            
-@app.route("/orders", methods=["POST", "GET"])
-def loadorder():
-    return render_template ("loadorder.html")
+@app.route("/success/<orderid>")
+def success(orderid):
+    return render_template ("success.html", orderid == orderid)
+
+
+@app.route("/loadorders/<id>", methods=["POST", "GET"])
+def loadorder(id):
+    data = db.get_all_order_data(id);
+    comment = request.args.get("comment")
+    submitc = request.args.get("submitc")
+    if (submitc == "Submit" and comment != ""):
+        db.add_comment(comment, id)
+        comment = ""
+        return redirect ("/loadorders/" + str(id))
+    return render_template ("loadorder.html", username=  )
 
 @app.route("/orderspec", methods=["POST", "GET"])
-def specorder(stores, periods): ## one is null or not
-    orders = get_orders (stores, period)
-    return render_template ("specorder.html", orders);
+def specorder(): ## one is null or not
+    select = request.args.get("select")
+    search = request.args.get("search")
+    searchsubmit = request.args.get("searchsubmit")
+
+    if(searchsubmit == "searchsubmit"): 
+        if (select == "Period"):
+            orders = db.get_orders (0, search)
+        elif (select == "Store"):
+            orders = db.get_orders (search, 0)
+        else:
+            return redirect("/oprofile/" + str(search))
+    return render_template ("specorder.html", orders == orders);
 
 @app.route("/results", methods=["POST", "GET"])
 def results():
-    search = request.args.get("search");
-
+    search = request.args.get("search")
     if(search):
-        term = request.args.get("term");
-        loc = request.args.get("loc");
+        term = request.args.get("term")
+        loc = request.args.get("loc")
         results = yelp.search("restaurant","New York");
         if(results==None):
             flash("No results came up")
