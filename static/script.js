7e5d0a4f4edbb786f4339eedbfea448316874b00
David Bang <davidbang2012@gmail.com>
2015-01-26 15:32:33 -0500

    modified templates

--- static/script.js
+++ static/script.js
@@ -6,4 +6,5 @@ var log = function(d){
 $(document).ready(function() {
     h = document.getElementById('in').onclick = function(){ window.location = "/login";}
     m = document.getElementById('up').onclick = function(){window.location = "/register";}
+    n = document.getElementById('out').onclick = function(){window.location = "../logout";}
 });
