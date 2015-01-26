3b0556a4add49e9522c63ffd6c6aa00c5c10a4af
Anish Malhotra <anishmalhotra97@gmail.com>
2015-01-26 14:37:43 -0800

    fix

--- static/script.js
+++ static/script.js
@@ -1,14 +1,13 @@
 
 var log = function(d){
-		console.log(d);
+    console.log(d);
 };
-
 $(document).ready(function() {
     h = document.getElementById('in').onclick = function(){ window.location = "/login";}
     m = document.getElementById('up').onclick = function(){window.location = "/register";}
-    n = document.getElementById('out').onclick = function(){window.location = "../logout";}
+    n = document.getElementById('out').onclick = function(){window.location = "/logout";}
+    o = document.getElementById('userb').onclick = function(){window.location = "/profile/" + document.getElementById("userb").innerHTML;}
 });
-
-$('.col-sm-8 col-md-6').click(function() { 
-    window.location.href = "http://stackoverflow.com";
+$('.col-sm-8 col-md-6').click(function() {
+    window.location.href = "/loadorders/" + document.getElementByID(/*id*/).innerHTML;
 }).css("cursor","pointer");
