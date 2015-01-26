0dd0afe4013be4ee43d4423524154fc2ec311693
David Bang <davidbang2012@gmail.com>
2015-01-26 17:15:29 -0500

    script js

--- static/script.js
+++ static/script.js
@@ -6,7 +6,8 @@ var log = function(d){
 $(document).ready(function() {
     h = document.getElementById('in').onclick = function(){ window.location = "/login";}
     m = document.getElementById('up').onclick = function(){window.location = "/register";}
-    n = document.getElementById('out').onclick = function(){window.location = "../logout";}
+    n = document.getElementById('out').onclick = function(){window.location = "/logout";}
+    o = document.getElementById('userb').onclick = function(){window.location = "/profile/" + document.getElementById("userb").innerHTML;}
 });
 
 $('.col-sm-8 col-md-6').click(function() { 
